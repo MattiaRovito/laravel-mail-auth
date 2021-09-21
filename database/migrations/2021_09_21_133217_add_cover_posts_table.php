d91f2ff68ee45fc68595e96a23ea1141f976ea01
MattiaR <rovitomattia91@gmail.com>
2021-09-21 15:34:15 +0200

    popolato la cover_posts

---
+++ database/migrations/2021_09_21_133217_add_cover_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCoverPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // creo la colonna cover con valore NULL prima della colonna slug
+            $table->string('cover')->nullable()->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            
+            // Per un eventuale rollback
+            $table->dropColumn('cover'); 
+
+
+        });
+    }
+}
